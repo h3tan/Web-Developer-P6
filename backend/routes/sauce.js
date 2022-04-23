aa632442ed20db3f3112529c211aed35053c826a
Herve Thi <thi.h3rve@gmail.com>
2022-04-23 23:34:19 +0200

    Construction du parcours Sauce: modèle, contrôleur et route

---
+++ backend/routes/sauce.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middlewares/auth");
+const multer = require("../middlewares/multer-config");
+const sauceCtrl = require("../controllers/sauce");
+
+router.post("/", auth, multer, sauceCtrl.createSauce);
+
+router.get("/", auth, sauceCtrl.getAllSauce);
+
+router.get("/:id", auth, sauceCtrl.getOneSauce);
+
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+
+module.exports = router;
