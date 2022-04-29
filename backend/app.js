4b23f490ec49d4caf4895470a922232227c45314
Herve Thi <thi.h3rve@gmail.com>
2022-04-29 21:16:39 +0200

    Mise en place des variables d'environnement et de helmet

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
 const path = require("path");
 const app = express();
+const helmet = require("helmet");
+
+// Utilisation des variables d'environnement
+const MY_ID = process.env.MONGODB_ID;
+const MY_PASSWORD = process.env.MONGODB_PASSWORD;
+const MY_CLUSTER = process.env.MONGODB_CLUSTER_NAME;
+const MY_DATABASE = process.env.MONGODB_DATABASE_NAME;
 
 mongoose
   .connect(
-    "mongodb+srv://Hetan:YotsubaNakano@cluster0.wqmvs.mongodb.net/test?retryWrites=true&w=majority",
+    `mongodb+srv://${MY_ID}:${MY_PASSWORD}@${MY_CLUSTER}.wqmvs.mongodb.net/${MY_DATABASE}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use(express.json());
+app.use(helmet());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
